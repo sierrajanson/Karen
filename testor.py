be4ac6bdddf3d5d3b6b00d3938fd88c55dfa1938
Sankie005 <sankritya0@gmail.com>
2023-12-28 13:30:01 -0800

    Added Testor And Virtualenvscript

---
+++ testor.py
@@ -0,0 +1,25 @@
+## This is a testor python file containing the testor class used to check the accuracy of our model 
+from transformers import pipeline
+class Testor():
+    def __init__(self,statement,answer):
+        self.answer=answer
+        self.statement=statement
+        self.classifier = pipeline(
+    model="lxyuan/distilbert-base-multilingual-cased-sentiments-student", 
+    return_all_scores=True)
+    def check(self):
+        answer=self.answers
+        statement=self.statement
+        classifier=self.classifier
+        highest_val=["default",0]
+        for i in classifier(statement)[0]:
+            if i.get('score')>=highest_val[-1]:
+                highest_val=[f"{i.get('label')}",f"{i.get('score')}"]
+        if answer!=highest_val[0]:
+            return f"Failure: Expected answer:{highest_val[0]},got answer {answer}"
+        else: 
+            return f"Correct Answer"
+
+            
+
+        
---
+++ virtualenvscript.bat
@@ -0,0 +1,24 @@
+REM Set the name of the virtual environment
+set VENV_NAME=venv
+
+REM Set the Python executable path (modify if needed)
+set PYTHON_EXECUTABLE=python
+
+REM Create virtual environment
+%PYTHON_EXECUTABLE% -m venv %VENV_NAME%
+
+REM Activate virtual environment
+if exist "%VENV_NAME%\Scripts\activate" (
+    call "%VENV_NAME%\Scripts\activate"
+) else (
+    echo Unable to activate virtual environment.
+)
+
+REM Optional: Install any required packages
+if defined VENV_NAME (
+    echo Installing required packages...
+    pip install -r requirements.txt
+)
+
+echo Virtual environment is activated. Use "deactivate" to exit.
+exit /b 0
