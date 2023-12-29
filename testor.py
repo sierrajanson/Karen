6eb3fcbe9936d6bbda6186d360edcf10275f2869
Sankie005 <sankritya0@gmail.com>
2023-12-28 16:22:21 -0800

    Added Testorv2 for batch testing

--- testor.py
+++ testor.py
@@ -16,3 +16,27 @@ class Testor():
             print(f"Test Passed Confidence Value:{expected[0].get('score')//(1/100)}%")
         
 # Example usecase Testor('i love you','positive').check
+class Testorv2():
+    def __init__(self,linput) -> None:
+        self.linput=linput
+    @property
+    def check(self):
+        outputlist_failed=[]
+        for i in self.linput:
+            self.answer=i[1]
+            self.statement=i[0]
+            from transformers import pipeline
+            pipe = pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
+            expected=(pipe(self.statement))
+            userans=str(self.answer).upper()
+            if expected[0].get('label')!= userans:
+                print(f"Test Failed Expected:{expected[0].get('label')} and instead got:{userans}")
+                outputlist_failed.append([i,"expected:"+expected[0].get('label')])
+            else:
+                print(f"Test Passed Confidence Value:{expected[0].get('score')//(1/100)}%")
+        if outputlist_failed==[]:
+            print("No failures.. All Cases Pass")
+        else:
+            print(f"Number of failures:{len(outputlist_failed)}")
+            print(f"Output list of all the failures:{outputlist_failed}")
+# Example Use Case Testorv2([('i love you','positive'),('i love you','negative')]).check
