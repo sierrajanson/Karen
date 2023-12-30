bc394933f855beb15f4e3199c2891dd6f78b15d4
Sierra Janson <60485703+sierrajanson@users.noreply.github.com>
2023-12-29 16:31:40 -0800

    changed POST to GET so can integrate into React
    - commented out index.html briefly to avoid conflicts, will restore asap
    - added origin list to avoid CORS error

--- app/main.py
+++ app/main.py
@@ -1,27 +1,39 @@
 from fastapi import FastAPI, HTTPException,APIRouter,Request
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
-from typing import List
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse 
 import os
+from pydantic import BaseModel
+from typing import List
 app = FastAPI()
-os.environ['SENTENCE_TRANSFORMERS_HOME'] = './.cache' ## For Docker 
-app.mount("/files/", StaticFiles(directory='../app'), name="index")
-class TextInput(BaseModel):
-    InputText: str # python casing??????
+# os.environ['SENTENCE_TRANSFORMERS_HOME'] = './.cache' ## For Docker 
+# app.mount("/files/", StaticFiles(directory='../app'), name="index")
+origins = ["*"]
+    # 'http://localhost:3000'
+#]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
+# class TextInput(BaseModel):
+#     InputText: str 
 
 def emotion_detection(str1: str) -> str:
     from transformers import pipeline
     pipe = pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
     expected=(pipe(str1))
-    return expected[0].get('label')    
-@app.get("/")
-async def read_index():
-    return FileResponse('index.html')
-@app.post("/generate-emotion/")
-async def detect_emotion(input_data: TextInput):
-    text1 = input_data.InputText
-    emotion= emotion_detection(text1)
-    response={"Text Entered":text1,"emotion":emotion}
+    return expected[0].get('label')  
+# @app.get("/")
+# async def read_index():
+#     return FileResponse('index.html')
+
+@app.get("/testing/")
+async def root(input_data):
+    emotion= emotion_detection(input_data)
+    response={"Text Entered":input_data,"emotion":emotion}
     return response
+
+    
+  
