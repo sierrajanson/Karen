6232829c4ebc68bf6ff4ab9c926ce7360dd9557d
Sierra Janson <60485703+sierrajanson@users.noreply.github.com>
2023-12-29 16:24:41 -0800

    Add files via upload
    integrated API

--- name-app/src/App.js
+++ name-app/src/App.js
@@ -1,12 +1,11 @@
 import Karen from './components/Karen.js';
 import './App.css';
-
-
+import React, { useEffect } from 'react'
 
 function App() {
   return (
     <div className="App">
-      {<Karen/>}
+      <Karen/>
     </div>
   );
 }
--- name-app/src/components/Karen.js
+++ name-app/src/components/Karen.js
@@ -1,7 +1,7 @@
-import {React, useEffect} from 'react';
+import {React, useState, useEffect} from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import '../App.css';
-
+import api from '../api';
 
 const Karen = () =>{
   const {
@@ -11,16 +11,30 @@ const Karen = () =>{
       browserSupportsSpeechRecognition
   } = useSpeechRecognition();
 
+
+    const [data, setData] = useState(null);
     useEffect(()=>{     // on change, log what is being said to verify that microphone is working
       console.log(transcript);
+      console.log(data);
     })
+
+    const fetchDat = async () => {
+      const resp = await api.get('/testing/',{
+        params: {input_data:'sunshine and rainbows'},
+      });
+      console.log(resp.data);
+    }
+
+    useEffect(()=>{
+      fetchDat();
+    },[]);
+
     return ( // can give startListening method argument of "{continuous: true}" so it doesn't listening after it detects that you've stopped speaking
     <div >
       <div className="microphone_buttons"> 
-        <button onClick={SpeechRecognition.startListening}>Start</button>
         <button onClick={SpeechRecognition.stopListening}>Stop</button>
       </div>
-      <h1> :) </h1>
+      <h1> :() </h1>
     </div>
   );
 }
